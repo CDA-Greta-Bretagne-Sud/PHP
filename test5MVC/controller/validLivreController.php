27bfc8a34926a3e0826772d5f05f055162fba789
Casinni <pascal@lamy.mobi>
2022-12-02 11:23:34 +0100

    test5 MVC
    Le dossier modele a été intégré.Les dossiers metier et PDO on été copiés dasn le dossier modele.
    PDO a été renommé en DAO (Data Access Object)

---
+++ test5MVC/controller/validLivreController.php
@@ -0,0 +1,16 @@
+<?php
+
+class validLivreController
+{
+    public function __construct()
+    {
+        session_start();
+        error_reporting(0);
+//TODO
+        if ($operation == "insert") {
+        } else {
+        //erreur on renvoit à la page d'accueil
+                header('Location: accueil.php?id=' . $_SESSION["token"]);
+        }
+    }
+}
