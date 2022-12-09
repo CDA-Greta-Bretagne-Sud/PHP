a855f5fe3f7a1c3c8b12994dc902cda98ce89e4b
muneth <muneetchaudhary@me.com>
2022-12-09 15:19:04 +0100

    openai both

--- openai_QR_nodejs/routes/openaiRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { generateAnswer1 } = require("../controllers/openaiController");
-const router = express.Router();
-
-router.post("/generateAnswer", generateAnswer1);
-
-module.exports = router;
